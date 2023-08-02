10bdd1990c58ab640e5b9e46dc8392eb3591128b
Oblivion <fengredrum@163.com>
2023-08-02 16:46:18 +0800

    Evaluating CER

--- eval.py
+++ eval.py
@@ -1,62 +1,45 @@
-import gc
-import numpy as np
 import torch
+import numpy as np
 import evaluate
+import gc
 
-from transformers import WhisperProcessor, WhisperTokenizer, WhisperFeatureExtractor, WhisperForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer, Seq2SeqTrainer
-from peft import prepare_model_for_int8_training, LoraConfig, PeftModel, LoraModel, LoraConfig, get_peft_model
+from transformers import WhisperProcessor, WhisperTokenizer, WhisperFeatureExtractor, WhisperForConditionalGeneration
 from datasets import load_dataset, DatasetDict, Audio
-from dataclasses import dataclass
 from torch.utils.data import DataLoader
+from dataclasses import dataclass
 from typing import Any, Dict, List, Union
 from tqdm import tqdm
 
 metric = evaluate.load("cer")
-experiment_name = "whisper-small-cantonese"
-use_lora = True
-model_name_or_path = "openai/whisper-small"
+model_name_or_path = "./whisper-small-cantonese/"
+ckpt_path = "checkpoint-1000"
 language = "zh"
 language_abbr = "zh-HK"
 task = "transcribe"
 dataset_name = "common_voice"
+batch_size = 32
 
+# TODO 8-bit training and inference very slow
+model = WhisperForConditionalGeneration.from_pretrained(
+    model_name_or_path + ckpt_path,
+    # load_in_8bit=True,
+    device_map="auto")
+model.config.forced_decoder_ids = None
+model.config.suppress_tokens = []
+tokenizer = WhisperTokenizer.from_pretrained(
+    model_name_or_path, language=language, task=task)
+feature_extractor = WhisperFeatureExtractor.from_pretrained(model_name_or_path)
+processor = WhisperProcessor.from_pretrained(
+    model_name_or_path, language=language, task=task)
 
 common_voice = DatasetDict()
-common_voice["train"] = load_dataset(
-    dataset_name, language_abbr, split="train+validation")
 common_voice["test"] = load_dataset(
-    dataset_name, language_abbr, split="test[:1000]")
-
+    dataset_name, language_abbr, split="test[:100]")
 common_voice = common_voice.remove_columns(
     ["accent", "age", "client_id", "down_votes",
         "gender", "locale", "path", "segment", "up_votes"]
 )
 print(common_voice)
-
-feature_extractor = WhisperFeatureExtractor.from_pretrained(model_name_or_path)
-tokenizer = WhisperTokenizer.from_pretrained(
-    model_name_or_path, language=language, task=task)
-processor = WhisperProcessor.from_pretrained(
-    model_name_or_path, language=language, task=task)
-
-# TODO 8-bit training and inference very slow
-model = WhisperForConditionalGeneration.from_pretrained(
-    model_name_or_path,
-    load_in_8bit=True,
-    device_map="auto")
-model.config.forced_decoder_ids = None
-model.config.suppress_tokens = []
-# silence the warnings. Please re-enable for inference!
-model.config.use_cache = False
-model = prepare_model_for_int8_training(model)
-
-if use_lora:
-    config = LoraConfig(r=32, lora_alpha=64, target_modules=[
-                        "q_proj", "v_proj"], lora_dropout=0.05, bias="none")
-    model = get_peft_model(model, config)
-    model.print_trainable_parameters()
-
-
 common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
 
 
@@ -74,7 +57,7 @@ def prepare_dataset(batch):
 
 
 common_voice = common_voice.map(
-    prepare_dataset, remove_columns=common_voice.column_names["train"], num_proc=2)
+    prepare_dataset, remove_columns=common_voice.column_names["test"], num_proc=2)
 
 
 @dataclass
@@ -111,68 +94,8 @@ class DataCollatorSpeechSeq2SeqWithPadding:
 
 
 data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
-
-
-def compute_metrics(pred):
-    pred_ids = pred.predictions
-    label_ids = pred.label_ids
-
-    # replace -100 with the pad_token_id
-    label_ids[label_ids == -100] = tokenizer.pad_token_id
-
-    # we do not want to group tokens when computing the metrics
-    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
-    label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
-
-    cer = 100 * metric.compute(predictions=pred_str, references=label_str)
-
-    return {"cer": cer}
-
-
-training_args = Seq2SeqTrainingArguments(
-    output_dir="./logs/" + experiment_name,  # change to a repo name of your choice
-    per_device_train_batch_size=64,
-    gradient_accumulation_steps=2,  # increase by 2x for every 2x decrease in batch size
-    learning_rate=5e-5,
-    warmup_steps=500,
-    max_steps=20000,
-    evaluation_strategy="steps",
-    gradient_checkpointing=True,
-    optim="adamw_torch",
-    fp16=True,
-    per_device_eval_batch_size=8,
-    generation_max_length=225,
-    save_steps=1000,
-    eval_steps=500,
-    logging_steps=25,
-    report_to=["tensorboard"],
-    predict_with_generate=True,
-    # load_best_model_at_end=True,
-    # metric_for_best_model="cer",
-    greater_is_better=False,
-    # required as the PeftModel forward doesn't have the signature of the wrapped model's forward
-    remove_unused_columns=False,
-    label_names=["labels"],  # same reason as above
-    push_to_hub=False,
-)
-
-
-trainer = Seq2SeqTrainer(
-    args=training_args,
-    model=model,
-    train_dataset=common_voice["train"],
-    eval_dataset=common_voice["test"],
-    data_collator=data_collator,
-    # compute_metrics=compute_metrics,
-    tokenizer=processor.feature_extractor,
-)
-
-processor.save_pretrained(training_args.output_dir)
-trainer.train()
-
-
 eval_dataloader = DataLoader(
-    common_voice["test"], batch_size=32, collate_fn=data_collator)
+    common_voice["test"], batch_size=batch_size, collate_fn=data_collator)
 
 model.eval()
 for step, batch in enumerate(tqdm(eval_dataloader)):
