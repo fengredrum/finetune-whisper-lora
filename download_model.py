a8dd511e1c4a0fbc4cc334655aaf34611a733948
Oblivion <fengredrum@163.com>
2023-08-04 23:35:35 +0800

    Create download_model.py

---
+++ download_model.py
@@ -0,0 +1,9 @@
+from huggingface_hub import snapshot_download
+
+repo_id = "openai/whisper-small"
+root_dir = "./hf_hub/models/"
+
+snapshot_download(repo_id=repo_id,
+                  ignore_patterns=["*.msgpack", "*.h5"],
+                  local_dir=root_dir + repo_id,
+                  local_dir_use_symlinks=False)
