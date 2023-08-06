de5c38876835178f3ed90ea87ad5e7b8f5454afb
Oblivion <fengredrum@163.com>
2023-08-06 22:09:05 +0800

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -4,6 +4,6 @@ repo_id = "openai/whisper-small"
 root_dir = "./hf_hub/models/"
 
 snapshot_download(repo_id=repo_id,
-                  ignore_patterns=["*.msgpack", "*.h5"],
+                  ignore_patterns=["*.msgpack", "*.h5", "*.pt"],
                   local_dir=root_dir + repo_id,
                   local_dir_use_symlinks=False)
