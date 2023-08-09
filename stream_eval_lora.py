44eac6e5cd55652f0ece1c0b328e4bf3e99049fb
Oblivion <fengredrum@163.com>
2023-08-09 22:23:28 +0800

    First worked version

--- stream_eval_lora.py
+++ stream_eval_lora.py
@@ -3,9 +3,9 @@ import numpy as np
 import evaluate
 import gc
 
-from transformers import WhisperProcessor, WhisperTokenizer, WhisperForConditionalGeneration, WhisperFeatureExtractor
+from transformers import WhisperProcessor, WhisperTokenizer, WhisperForConditionalGeneration
 from peft import PeftModel, PeftConfig
-from datasets import load_dataset, load_from_disk, DatasetDict, Dataset, Audio
+from datasets import load_dataset, Dataset, Audio
 from torch.utils.data import DataLoader
 from dataclasses import dataclass
 from typing import Any, Dict, List, Union
@@ -17,9 +17,8 @@ task = "transcribe"
 metric = evaluate.load("cer")
 language = "zh"
 # Dataset setups
-dataset_name = "mozilla-foundation/common_voice_11_0"
-language_abbr = "zh-HK"
-saved_dir = "./hf_hub/datasets/" + dataset_name + "/" + language_abbr
+dataset_name = "mdcc"
+dataset_dir = "./datasets/" + dataset_name + "/"
 num_samples = 1000
 batch_size = 32
 
@@ -57,37 +56,35 @@ def prepare_dataset(batch):
     return batch
 
 
-filelist = f"./cnt_asr_train_metadata.csv"
-filepaths_and_text = load_filepaths_and_text(filelist)
-filepaths_and_text = filepaths_and_text[:1000]
-
+filelist = dataset_dir + "cnt_asr_test_metadata.csv"
+filepaths_and_text = load_filepaths_and_text(filelist)[:num_samples]
 filepaths_and_text[0].append("transcription")
 audio_paths, transcription_texts = [], []
+
 for i in range(1, len(filepaths_and_text)):
-    audio_path = filepaths_and_text[i][0]
+    audio_path = dataset_dir + filepaths_and_text[i][0][2:]
     audio_paths.append(audio_path)
 
-    transcription_path = filepaths_and_text[i][1]
+    transcription_path = dataset_dir + filepaths_and_text[i][1][2:]
     with open(transcription_path, encoding='utf-8') as f:
         transcription = [line.strip() for line in f][0]
     filepaths_and_text[i].append(transcription)
     transcription_texts.append(transcription)
 
 dataset_dict = {"audio": audio_paths, "sentence": transcription_texts}
-ds_train = Dataset.from_dict(dataset_dict)
-ds_train.to_json(saved_dir + "/train.json")
+ds_test = Dataset.from_dict(dataset_dict)
+ds_test.to_json(dataset_dir + "test.json")
 
-ds_train = load_dataset("json", data_files=saved_dir + "/train.json", split='train',
-                        streaming=True, features=ds_train.features)
+ds_test = load_dataset("json", data_files=dataset_dir + "/test.json", split='train',
+                       streaming=True, features=ds_test.features)
 
-ds_train = ds_train.cast_column("audio", Audio(sampling_rate=16000))
-ds_train = ds_train.map(
-    prepare_dataset, remove_columns=ds_train.column_names)
+ds_test = ds_test.cast_column("audio", Audio(sampling_rate=16000))
+ds_test = ds_test.map(prepare_dataset, remove_columns=list(
+    next(iter(ds_test)).keys())).with_format("torch")
 
-print(next(iter(ds_train)))
-
-seed, buffer_size = 42, 10_000
-ds_train = ds_train.shuffle(seed, buffer_size=buffer_size)
+seed, buffer_size = 42, 500
+ds_test = ds_test.shuffle(seed, buffer_size=buffer_size)
+print(next(iter(ds_test)))
 
 
 @dataclass
@@ -125,8 +122,9 @@ class DataCollatorSpeechSeq2SeqWithPadding:
 
 data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
 eval_dataloader = DataLoader(
-    ds_train, batch_size=batch_size, collate_fn=data_collator)
-
+    ds_test, batch_size=batch_size, collate_fn=data_collator)
+forced_decoder_ids = processor.get_decoder_prompt_ids(
+    language=language, task=task)
 
 model.eval()
 for step, batch in enumerate(tqdm(eval_dataloader)):
@@ -135,11 +133,9 @@ for step, batch in enumerate(tqdm(eval_dataloader)):
             generated_tokens = (
                 model.generate(
                     input_features=batch["input_features"].to("cuda"),
-                    decoder_input_ids=batch["labels"][:, :4].to("cuda"),
+                    forced_decoder_ids=forced_decoder_ids,
                     max_new_tokens=255,
-                )
-                .cpu()
-                .numpy()
+                ).cpu().numpy()
             )
             labels = batch["labels"].cpu().numpy()
             labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
