d80b379592a97b3569007ca1037f30511223670d
Oblivion <fengredrum@163.com>
2023-09-10 11:31:49 +0800

    Update aishell_1.sh

--- dataset_scripts/aishell_1.sh
+++ dataset_scripts/aishell_1.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-cd ./datasets/aishell-1/data_aishell/wav
+cd ..
+mkdir -p datasets/aishell_1 && cd datasets/aishell_1
+wget https://us.openslr.org/resources/33/data_aishell.tgz
+tar -zxf data_aishell.tgz && rm data_aishell.tgz
+cd data_aishell/wav
 for wav in ./*.tar.gz; do
     echo "Extracting wav from $wav"
     tar -zxf $wav && rm $wav
