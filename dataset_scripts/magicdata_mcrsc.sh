3f5c8d16259cc516978de498c02813a600b6c442
Oblivion <fengredrum@163.com>
2023-09-10 11:32:02 +0800

    Update magicdata_mcrsc.sh

---
+++ dataset_scripts/magicdata_mcrsc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ..
+mkdir -p datasets/magicdata && cd datasets/magicdata
+wget https://us.openslr.org/resources/68/train_set.tar.gz
+tar -zxf train_set.tar.gz && rm train_set.tar.gz
+wget https://us.openslr.org/resources/68/test_set.tar.gz
+tar -zxf test_set.tar.gz && rm test_set.tar.gz
