655fe00272a616f591e36a7d0ae9886580795268
Oblivion <fengredrum@163.com>
2023-10-08 11:28:05 +0800

    Update infer.py

--- convert_model/using_olive/infer.py
+++ convert_model/using_olive/infer.py
@@ -1,17 +1,18 @@
 import os
+import librosa
+import numpy as np
+import onnxruntime
+
+from onnxruntime_extensions import get_library_path
+from datetime import datetime
+
 os.environ["OMP_NUM_THREADS"] = "1"  # export OMP_NUM_THREADS=1
 os.environ["OPENBLAS_NUM_THREADS"] = "1"  # export OPENBLAS_NUM_THREADS=1
 os.environ["MKL_NUM_THREADS"] = "1"  # export MKL_NUM_THREADS=1
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"  # export VECLIB_MAXIMUM_THREADS=1
 os.environ["NUMEXPR_NUM_THREADS"] = "1"  # export NUMEXPR_NUM_THREADS=1
-os.environ["inter_op_num_threads"] = "1"
-os.environ["intra_op_num_threads"] = "1"
 
-from datetime import datetime
-import numpy as np
-import librosa
-import onnxruntime
-from onnxruntime_extensions import get_library_path
+num_threads = 2
 
 audio_file = "data/test.wav"
 model = "models/conversion-transformers_optimization-onnx_dynamic_quantization-insert_beam_search-prepost/whisper_cpu_int8_cpu-cpu_model.onnx"
@@ -30,14 +31,15 @@ inputs = {
     # "attention_mask": np.zeros((1, 80, 3000), dtype=np.int32),
 }
 
-num_threads = 2
+
 options = onnxruntime.SessionOptions()
 options.register_custom_ops_library(get_library_path())
 options.intra_op_num_threads = num_threads
 options.inter_op_num_threads = num_threads
 options.execution_mode = onnxruntime.ExecutionMode.ORT_PARALLEL
 options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-session = onnxruntime.InferenceSession(model, options, providers=["CPUExecutionProvider"])
+session = onnxruntime.InferenceSession(
+    model, options, providers=["CPUExecutionProvider"])
 
 start_optimized = datetime.now()
 num_inferences = 5
@@ -47,6 +49,6 @@ end_optimized = datetime.now()
 
 print(outputs)
 
-optimized_inference_time = (
+inference_time = (
     end_optimized - start_optimized).total_seconds() / num_inferences
-print(f"Inference time: {optimized_inference_time} seconds.")
+print(f"Inference time: {inference_time} seconds.")
